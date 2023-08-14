1a7ad4e2eed68adcb1e9e0bd84fa28a84cf376bb
Joey Gibbons <gibbonsjoey95@gmail.com>
2023-08-14 09:38:29 -0500

    re did selection sort

--- selection_sort.py
+++ selection_sort.py
@@ -1,17 +1,18 @@
 def findSmallest(arr):
   smallest = arr[0]
   smallest_index = 0
-  for i in range(1, len(arr)):
+  for i in range (1, len(arr)):
     if arr[i] < smallest:
       smallest = arr[i]
       smallest_index = i
   return smallest_index
 
 def selectionSort(arr):
-  newArr = []
-  for i in range(len(arr)):
+  new_array = []
+  for i in range (len(arr)):
     smallest = findSmallest(arr)
-    newArr.append(arr.pop(smallest))
-    return newArr
+    new_array.append(arr.pop(smallest))
+  return new_array
 
+print(findSmallest([5,3,6,2,10]))
 print(selectionSort([5,3,6,2,10]))
